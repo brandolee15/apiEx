431e4f08c7c87892ed10f995bc426af265cc36ef
Julian James <julian@Julians-MacBook-Pro.local>
2021-12-03 14:37:56 +0000

    updated controllers avatar.js

---
+++ controllers/avatar.js
@@ -0,0 +1,25 @@
+//controller
+
+const express = require('express')
+const router = express.Router();
+const avatars = require('../data');
+const Avatar = require('../models/avatar')
+
+// app.get('/cats', (req, res) => {
+//     res.send(cats);
+// });
+
+router.get('/', (req, res) => {
+    const avatars = Avatar.all;
+    res.send(avatars);
+});
+
+router.get('/:id', (req, res) => {
+    const avatarId = parseInt(req.params.id);
+    const selectedAvatar = Avatar.findById(avatarId);
+
+    res.send(selectedAvatar);
+});
+
+
+module.exports = router;
